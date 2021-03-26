49f0644805bc6754fc9918147e85708ad52b0b73
Kun Zhang <zhangkun322@qq.com>
2021-03-26 15:43:44 +0800

    Update sw_balance.m

--- sw_balance.m
+++ sw_balance.m
@@ -33,6 +33,9 @@ function [Tr, Es, wa, theta_new, DeepDrain] = sw_balance(Topt, Ta, IWS, pTr_ly, 
 
         % layer #1 new soil water content in unsaturated zone,
         % and possible exceeded water downward to layer #2
+
+        % existed water column
+        d1.*wo1;
         w1_unsat = wo(1) + IWS ./ (d1 .* whc); % soil water content of unsaturated zone
         
         if w1_unsat > 1
